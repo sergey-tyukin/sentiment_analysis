bb4ceac7142aecebd18eca67fac7c9cc001d806a
Sergey <SVTyukin@yandex.ru>
2026-02-11 07:24:52 +0300

    feat: добавлен расчёт спреда по компаниям

--- src/03_quotes_analysis.R
+++ src/03_quotes_analysis.R
@@ -13,11 +13,14 @@ get_stats <- function(asset) {
     max     = max(asset_ret),
     skewness = moments::skewness(asset_ret),
     kurtosis = moments::kurtosis(asset_ret),
-    volatility = sd(asset_ret)  # sd() использует формулу с (n-1)
+    volatility = sd(asset_ret),  # sd() использует формулу с (n-1)
+    spread_bbo = median(asset$spread_bbo, na.rm = TRUE),
+    spread_lv10 = median(asset$spread_lv10, na.rm = TRUE),
+    spread_1mio = median(asset$spread_1mio, na.rm = TRUE)
   )
 }
 
-raw_quotes <- readr::read_rds(here("data", "processed", "raw_quotes.rds"))
-securities_stats <- purrr::map_dfr(raw_quotes, get_stats, .id = "ticker")
+raw_quotes_spread <- readr::read_rds(here("data", "processed", "raw_quotes_spread.rds"))
+securities_stats <- purrr::map_dfr(raw_quotes_spread, get_stats, .id = "ticker")
 
 write_xlsx(securities_stats, here("output", "tables", "Securities_stats.xlsx"))
